49fa1047fda125f350a731617c0b55070c4dc2a0
XanderB88 <alexxanderbykov@gmail.com>
2021-09-21 06:54:09 +0500

    Work with a button and a label.

--- UIKit elements(learning project)/ViewController.swift
+++ UIKit elements(learning project)/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var buttonLabel: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textLabel.isHidden = true
+        textLabel.font = textLabel.font.withSize(14)
+        textLabel.textColor = .white
+        
+        buttonLabel.setTitle("Show label", for: .normal)
+        buttonLabel.setTitleColor(.white, for: .normal)
+        buttonLabel.backgroundColor = .black
+        buttonLabel.layer.cornerRadius = 5
+        
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        if textLabel.isHidden {
+            textLabel.isHidden = false
+            textLabel.text = "Show"
+            
+            buttonLabel.setTitle("Clear", for: .normal)
+            buttonLabel.backgroundColor = .red
+        } else {
+            textLabel.isHidden = true
+            
+            buttonLabel.setTitle("Show label", for: .normal)
+            buttonLabel.backgroundColor = .black
+            textLabel.isHidden = true
+        }
+    }
+    
 }
 
