b190d74be1ef3acf91b33e618658e318795c814f
XanderB88 <alexxanderbykov@gmail.com>
2021-09-23 04:43:52 +0500

    Work with segment control and label.

--- UIKit elements(learning project)/ViewController.swift
+++ UIKit elements(learning project)/ViewController.swift
@@ -8,24 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var textLabel: UILabel!
     @IBOutlet weak var buttonLabel: UIButton!
     
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var segmentLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        //Label
         textLabel.isHidden = true
         textLabel.font = textLabel.font.withSize(14)
         textLabel.textColor = .white
-        
+        //Button
         buttonLabel.setTitle("Show label", for: .normal)
         buttonLabel.setTitleColor(.white, for: .normal)
         buttonLabel.backgroundColor = .black
         buttonLabel.layer.cornerRadius = 5
         
+//        segmentLabel
+        segmentLabel.text = "First segment is selected"
+        segmentLabel.textColor = .red
+        segmentLabel.font = segmentLabel.font.withSize(14)
+        segmentLabel.textAlignment = .center
+        segmentLabel.numberOfLines = 2
+        
+//        segmented control
+        segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: true)
     }
-
+    //Actions for button
     @IBAction func buttonPressed(_ sender: UIButton) {
         
         if textLabel.isHidden {
@@ -43,5 +55,23 @@ class ViewController: UIViewController {
         }
     }
     
+//    Actions for segmented control
+    @IBAction func chosenSegment(_ sender: UISegmentedControl) {
+        
+        switch segmentedControl.selectedSegmentIndex {
+            case 0:
+                segmentLabel.text = "First segment is selected"
+                segmentLabel.textColor = .red
+            case 1:
+                segmentLabel.text = "Second segment is selected"
+                segmentLabel.textColor = .blue
+            case 2:
+                segmentLabel.text = "Third segment is selected"
+                segmentLabel.textColor = .green
+            default:
+                print("Something went wrong")
+        }
+        
+    }
 }
 
